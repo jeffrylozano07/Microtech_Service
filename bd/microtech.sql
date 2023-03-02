eeaf90f27d4b68733c301df8e8cb1797a34dd23d
jeffrylozano07 <jeffryjlt07@gmail.com>
2023-03-01 22:09:03 -0500

    Proyecto Nube V2

--- bd/microtech.sql
+++ bd/microtech.sql
@@ -2,10 +2,10 @@
 -- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 01-03-2023 a las 19:55:31
--- Versión del servidor: 8.0.31
--- Versión de PHP: 8.0.26
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 02-03-2023 a las 04:05:58
+-- Versión del servidor: 10.4.24-MariaDB
+-- Versión de PHP: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -27,12 +27,10 @@ SET time_zone = "+00:00";
 -- Estructura de tabla para la tabla `categories`
 --
 
-DROP TABLE IF EXISTS `categories`;
-CREATE TABLE IF NOT EXISTS `categories` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `categories`
@@ -50,24 +48,27 @@ INSERT INTO `categories` (`id`, `name`) VALUES
 -- Estructura de tabla para la tabla `items`
 --
 
-DROP TABLE IF EXISTS `items`;
-CREATE TABLE IF NOT EXISTS `items` (
-  `id` int NOT NULL AUTO_INCREMENT,
+CREATE TABLE `items` (
+  `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
   `description` varchar(255) NOT NULL,
   `price` float NOT NULL,
   `image` varchar(255) NOT NULL,
-  `category` int NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `category` (`category`)
-) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=latin1;
+  `category` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `items`
 --
 
 INSERT INTO `items` (`id`, `name`, `description`, `price`, `image`, `category`) VALUES
-(40, 'Laptop HP Gamer', 'GAMING 15-DK1025LA 15.6 CORE I5-10300H 8GB 256GB SSD FREEDOS', 3800, 'HP_Gamer.jpg', 1);
+(40, 'Laptop HP Gamer', 'GAMING 15-DK1025LA 15.6 CORE I5-10300H 8GB 256GB SSD FREEDOS', 3800, 'HP_Gamer.jpg', 1),
+(41, 'Impresora Epson', 'Impresora Multifuncional', 650, '2004277231769_2.jpg', 5),
+(42, 'Laptop Lenovo', 'Core i7 10ma 16 ram 512 SSD', 3000, '2004298832174_2.jpg', 1),
+(43, 'AUDIFONO GAMER', 'LUCES RGB CON MICROFONO PC', 100, 'Imagenaudifono.jpg', 8),
+(44, 'PC COMPUTADORA RYZEN', 'RYZEN 5 5600G RAM 16GB SSD 240GB + HDD 1TB MONITOR 22 FULL HD', 2350, 'dddd.jpg', 7),
+(45, 'PC Gamer', 'Desktop HP Victus Gaming TG02-0014lam + Monitor HP X24ih Gaming', 3599, 'pc.jpg', 7),
+(46, 'Impresora multifuncional', 'Epson L3250 - negro', 750, 'impresora.jpg', 5);
 
 -- --------------------------------------------------------
 
@@ -75,13 +76,11 @@ INSERT INTO `items` (`id`, `name`, `description`, `price`, `image`, `category`) 
 -- Estructura de tabla para la tabla `user`
 --
 
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE IF NOT EXISTS `user` (
-  `Id` int NOT NULL AUTO_INCREMENT,
+CREATE TABLE `user` (
+  `Id` int(11) NOT NULL,
   `correo` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  PRIMARY KEY (`Id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `user`
@@ -89,6 +88,51 @@ CREATE TABLE IF NOT EXISTS `user` (
 
 INSERT INTO `user` (`Id`, `correo`, `password`) VALUES
 (1, 'nube@gmail.com', '123456');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `items`
+--
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `category` (`category`);
+
+--
+-- Indices de la tabla `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT de la tabla `items`
+--
+ALTER TABLE `items`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
+
+--
+-- AUTO_INCREMENT de la tabla `user`
+--
+ALTER TABLE `user`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Restricciones para tablas volcadas
